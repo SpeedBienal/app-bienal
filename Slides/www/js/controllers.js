80657a0ee8672c2a14149657f2d7dc14332c49cb
hackerzeta <pabloeferreyra@gmail.com>
2016-05-22 00:39:16 -0300

    pase directo de Pantalla 1 Slides

--- Slides/www/js/controllers.js
+++ Slides/www/js/controllers.js
@@ -1,7 +1,51 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['ionic'])
   
-.controller('homeCtrl', function($scope) {
+ .run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+  
+.controller('homeCtrl', function($scope, $ionicModal) {
+    $scope.allImages = [{
+        'src' : 'img/pic1.jpg'
+    }, {
+        'src' : 'img/pic2.jpg'
+    }, {
+        'src' : 'img/pic3.jpg'
+    }];
+    
+    $scope.testClick = function () {
+        confirm("Usted ha votado por esta foto\n"
+        +"muchas gracias");
+    };
+    
+    $scope.showImages = function(index) {
+        $scope.activeSlide = index;
+        $scope.showModal('templates/image-popover.html');
+    };
+    
+   $scope.showModal = function(templateUrl) {
+        $ionicModal.fromTemplateUrl(templateUrl, {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modal = modal;
+            $scope.modal.show();
+        });
+    };
 
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+        $scope.modal.remove();
+    };
 })
    
 .controller('cartCtrl', function($scope) {
@@ -10,5 +54,5 @@ angular.module('app.controllers', [])
    
 .controller('cloudCtrl', function($scope) {
 
-})
-    
+});
+  
