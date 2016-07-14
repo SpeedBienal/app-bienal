f09bfeea4bccb3cb8a2f6684afab72f76e608482
Pablo Ferreyra <pabloeferreyra@gmail.com>
2016-07-13 23:19:59 -0300

    modificacion controllers

--- sideMenu/www/js/controllers.js
+++ sideMenu/www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('app.controllers', [])
 
   //codigo feo por que nos pagaron poco por mucho xD
 .controller('inicioCtrl', function($scope,$ionicModal) {
-$scope.allImages = [{
+    $scope.allImages = [{
         'src' : 'img/pic1.jpg'
     }, {
         'src' : 'img/pic2.jpg'
@@ -21,26 +21,17 @@ $scope.allImages = [{
     $scope.testClick = function () {
         navigator.notification.confirm("Usted va a votar por este artista/obra", "Confirme Voto", ['OK', 'Cancel']);
       var btnIndex = buttonIndex;
-      switch(btnIndex)
+      if(btnIndex == 1)
       {
-        case 1:
-            window.plugins.toast.showLongBottom("Voto Registrado Con Exito!");
-            break
-        case 2:
-            window.plugins.toast.showLongBottom("Voto Cancelado");
-            break
-        default:
-            console.log("null");
-            break
+          window.plugins.toast.showLongBottom("Voto Registrado Con Exito!")
       }
     };
     
     $scope.showImages = function(index) {
         $scope.activeSlide = index;
         $scope.showModal('templates/image-popover.html');
-        
     };
-
+    
    $scope.showModal = function(templateUrl) {
         $ionicModal.fromTemplateUrl(templateUrl, {
             scope: $scope,
@@ -55,65 +46,10 @@ $scope.allImages = [{
         $scope.modal.hide();
         $scope.modal.remove();
     };
-
-    $scope.createCalendarEvent = function(index) {
-        switch(index)
-        {
-            case 0:
-                var title = 'Dia 1 interesante';
-                var loc = 'algun lugar del mundo';
-                var notes = 'My interesting Event notes.';
-                var startDate = new Date("July 14, 2016");
-                var endDate = new Date("July 14, 2016");
-                var calendarName = "MyCal";
-                var e = '';
-                console.log(index);
-                startDate.setMinutes(0);
-                endDate.setMinutes(0);
-                startDate.setSeconds(0);
-                break
-            case 1:
-                var title = 'Dia 2 interesante';
-                var loc = 'algun lugar del mundo';
-                var notes = 'My interesting Event notes.';
-                var startDate = new Date("July 14, 2016");
-                var endDate = new Date("July 14, 2016");
-                var calendarName = "MyCal";
-                var e = '';
-                console.log(index);
-                startDate.setMinutes(0);
-                endDate.setMinutes(0);
-                startDate.setSeconds(0);
-                break
-            case 2:
-                var title = 'Dia 3 interesante';
-                var loc = 'algun lugar del mundo';
-                var notes = 'My interesting Event notes.';
-                var startDate = new Date("July 14, 2016");
-                var endDate = new Date("July 14, 2016");
-                var calendarName = "MyCal";
-                var e = '';
-                console.log(index);
-                startDate.setMinutes(0);
-                endDate.setMinutes(0);
-                startDate.setSeconds(0);
-                break
-            default:
-                console.log("no mejoramos el codigo si no nos pagan mas");
-        }
-        try{
-            window.plugins.calendar.createEvent(title, loc, notes, startDate, endDate);
-            window.plugins.toast.showLongBottom('Añadido '+ title);
-            }
-            catch(e)
-            {
-                alert('error '+ e);
-            }     
-    }
 })
 
 .controller('musicaCtrl', function($scope, $ionicModal) {
-$scope.allImages = [{
+   $scope.allImages = [{
         'src' : 'img/pic1.jpg'
     }, {
         'src' : 'img/pic2.jpg'
@@ -134,15 +70,14 @@ $scope.allImages = [{
       var btnIndex = buttonIndex;
       switch(btnIndex)
       {
-        case 1:
-            window.plugins.toast.showLongBottom("Voto Registrado Con Exito!");
-            break
-        case 2:
-            window.plugins.toast.showLongBottom("Voto Cancelado");
-            break
-        default:
-            console.log("null");
-            break
+          case 1:
+          window.plugins.toast.showLongBottom("Voto Registrado Con Exito!");
+          break
+          case 2:
+          window.plugins.toast.showLongBottom("Voto Cancelado");
+          break
+          default:
+          console.log("nada");
       }
     };
     
@@ -245,15 +180,14 @@ $scope.allImages = [{
       var btnIndex = buttonIndex;
       switch(btnIndex)
       {
-        case 1:
-            window.plugins.toast.showLongBottom("Voto Registrado Con Exito!");
-            break
-        case 2:
-            window.plugins.toast.showLongBottom("Voto Cancelado");
-            break
-        default:
-            console.log("null");
-            break
+          case 1:
+          window.plugins.toast.showLongBottom("Voto Registrado Con Exito!");
+          break
+          case 2:
+          window.plugins.toast.showLongBottom("Voto Cancelado");
+          break
+          default:
+          console.log("nada");
       }
     };
     
@@ -335,7 +269,7 @@ $scope.allImages = [{
 })
    
 .controller('letrasCtrl', function($scope, $ionicModal) {
-$scope.allImages = [{
+   $scope.allImages = [{
         'src' : 'img/pic1.jpg'
     }, {
         'src' : 'img/pic2.jpg'
@@ -356,15 +290,14 @@ $scope.allImages = [{
       var btnIndex = buttonIndex;
       switch(btnIndex)
       {
-        case 1:
-            window.plugins.toast.showLongBottom("Voto Registrado Con Exito!");
-            break
-        case 2:
-            window.plugins.toast.showLongBottom("Voto Cancelado");
-            break
-        default:
-            console.log("null");
-            break
+          case 1:
+          window.plugins.toast.showLongBottom("Voto Registrado Con Exito!");
+          break
+          case 2:
+          window.plugins.toast.showLongBottom("Voto Cancelado");
+          break
+          default:
+          console.log("nada");
       }
     };
     
@@ -444,78 +377,8 @@ $scope.allImages = [{
             }     
     }
 })
-        $ionicModal.fromTemplateUrl(templateUrl, {
-            scope: $scope,
-            animation: 'slide-in-up'
-        }).then(function(modal) {
-            $scope.modal = modal;
-            $scope.modal.show();
-        });
-    };
-
-    $scope.closeModal = function() {
-        $scope.modal.hide();
-        $scope.modal.remove();
-    };
-
-    $scope.createCalendarEvent = function(index) {
-        switch(index)
-        {
-            case 0:
-                var title = 'Dia 1 interesante';
-                var loc = 'algun lugar del mundo';
-                var notes = 'My interesting Event notes.';
-                var startDate = new Date("July 14, 2016");
-                var endDate = new Date("July 14, 2016");
-                var calendarName = "MyCal";
-                var e = '';
-                console.log(index);
-                startDate.setMinutes(0);
-                endDate.setMinutes(0);
-                startDate.setSeconds(0);
-                break
-            case 1:
-                var title = 'Dia 2 interesante';
-                var loc = 'algun lugar del mundo';
-                var notes = 'My interesting Event notes.';
-                var startDate = new Date("July 14, 2016");
-                var endDate = new Date("July 14, 2016");
-                var calendarName = "MyCal";
-                var e = '';
-                console.log(index);
-                startDate.setMinutes(0);
-                endDate.setMinutes(0);
-                startDate.setSeconds(0);
-                break
-            case 2:
-                var title = 'Dia 3 interesante';
-                var loc = 'algun lugar del mundo';
-                var notes = 'My interesting Event notes.';
-                var startDate = new Date("July 14, 2016");
-                var endDate = new Date("July 14, 2016");
-                var calendarName = "MyCal";
-                var e = '';
-                console.log(index);
-                startDate.setMinutes(0);
-                endDate.setMinutes(0);
-                startDate.setSeconds(0);
-                break
-            default:
-                console.log("no mejoramos el codigo si no nos pagan mas");
-        }
-        try{
-            window.plugins.calendar.createEvent(title, loc, notes, startDate, endDate);
-            window.plugins.toast.showLongBottom('Añadido '+ title);
-            }
-            catch(e)
-            {
-                alert('error '+ e);
-            }     
-    }
-
-   
 .controller('visualesCtrl', function($scope, $ionicModal) {
-$scope.allImages = [{
+    $scope.allImages = [{
         'src' : 'img/pic1.jpg'
     }, {
         'src' : 'img/pic2.jpg'
@@ -536,15 +399,14 @@ $scope.allImages = [{
       var btnIndex = buttonIndex;
       switch(btnIndex)
       {
-        case 1:
-            window.plugins.toast.showLongBottom("Voto Registrado Con Exito!");
-            break
-        case 2:
-            window.plugins.toast.showLongBottom("Voto Cancelado");
-            break
-        default:
-            console.log("null");
-            break
+          case 1:
+          window.plugins.toast.showLongBottom("Voto Registrado Con Exito!");
+          break
+          case 2:
+          window.plugins.toast.showLongBottom("Voto Cancelado");
+          break
+          default:
+          console.log("nada");
       }
     };
     
@@ -626,7 +488,7 @@ $scope.allImages = [{
 })
    
 .controller('escenicasCtrl', function($scope, $ionicModal) {
-$scope.allImages = [{
+   $scope.allImages = [{
         'src' : 'img/pic1.jpg'
     }, {
         'src' : 'img/pic2.jpg'
@@ -647,15 +509,14 @@ $scope.allImages = [{
       var btnIndex = buttonIndex;
       switch(btnIndex)
       {
-        case 1:
-            window.plugins.toast.showLongBottom("Voto Registrado Con Exito!");
-            break
-        case 2:
-            window.plugins.toast.showLongBottom("Voto Cancelado");
-            break
-        default:
-            console.log("null");
-            break
+          case 1:
+          window.plugins.toast.showLongBottom("Voto Registrado Con Exito!");
+          break
+          case 2:
+          window.plugins.toast.showLongBottom("Voto Cancelado");
+          break
+          default:
+          console.log("nada");
       }
     };
     
